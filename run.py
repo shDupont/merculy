d40251ac7bc3b547b44d2ed4c2c463e8d245b530
GustavoHerreroNunes <ghnunes2010@gmail.com>
2025-09-01 09:58:59 -0300

    new azure 2

---
+++ run.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+import os
+import sys
+from src.main import app
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 8000))
+    app.run(host='0.0.0.0', port=port, debug=False)
